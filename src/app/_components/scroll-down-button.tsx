c9af90d60835b56f24eeeb16b073edb61ccc8e68
Noki-Asakuri <41738319+Noki-Asakuri@users.noreply.github.com>
2025-06-04 23:56:52 +0700

    ✨ feat(scroll-down-button): add chevron icon and improve styling

--- src/app/_components/scroll-down-button.tsx
+++ src/app/_components/scroll-down-button.tsx
@@ -1,6 +1,7 @@
 import { useChatStore } from "@/lib/chat/store";
 import { Button } from "./ui/button";
 import { cn } from "@/lib/utils";
+import { ChevronDownIcon } from "lucide-react";
 
 export function ScrollDownButton() {
   const isAtBottom = useChatStore((state) => state.isAtBottom);
@@ -11,16 +12,17 @@ export function ScrollDownButton() {
   }
 
   return (
-    <div className="pointer-events-none absolute -top-8 flex w-full items-center justify-center">
+    <div className="pointer-events-none absolute -top-10 flex w-full items-center justify-center">
       <Button
         className={cn(
-          "bg-muted/60 text-muted-foreground hover:bg-muted/80 border-border pointer-events-auto h-max w-30 cursor-pointer rounded-full border px-1.5 py-1 text-xs backdrop-blur-md transition-opacity",
+          "bg-muted/70 text-muted-foreground hover:bg-muted/90 border-border pointer-events-auto h-max w-max cursor-pointer rounded-full border px-1.5 py-1 text-xs backdrop-blur-md transition-opacity",
           { "pointer-events-none opacity-0": isAtBottom },
         )}
         type="button"
         onMouseDown={handleScrollDown}
       >
         Scroll to Bottom
+        <ChevronDownIcon />
       </Button>
     </div>
   );
