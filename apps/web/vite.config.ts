88b4b4974ab5da98551719f292035b4921e1f92e
Noki-Asakuri <41738319+Noki-Asakuri@users.noreply.github.com>
2026-03-13 00:49:46 +0700

    fix: revert @vitejs/plugin-react to v5 because v6 breaks React Compiler builds

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 
-import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
+import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
 
 export default defineConfig({
@@ -19,8 +18,7 @@ export default defineConfig({
     devtools({ consolePiping: { levels: ["log"] } }),
     nitro({ compatibilityDate: "latest" }),
     tanstackStart({ srcDirectory: "src", router: { routesDirectory: "app" } }),
-    viteReact(),
-    babel({ presets: [reactCompilerPreset] }),
+    viteReact({ babel: { plugins: ["babel-plugin-react-compiler"] } }),
     tailwindcss(),
   ],
 });
